545a1af8aecfeeb8fd77a803d4e2eb1e7f1936c0
Jason Wang <wangborong@cdjrlc.com>
2024-04-19 22:47:13 +0800

    Update crawl web function

--- main/crawl_web.c
+++ main/crawl_web.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include "freertos/FreeRTOS.h"
@@ -14,7 +15,8 @@
 
 #include "7-segment-led-display.h"
 
-#define MAX_HTTP_OUTPUT_BUFFER 3000
+#define CRAWL_PER_SEC 10
+#define MAX_HTTP_OUTPUT_BUFFER 2048
 static const char *TAG = "HTTP_CLIENT";
 
 #define WEB_SERVER "www.js-lottery.com"
@@ -22,9 +24,7 @@ static const char *TAG = "HTTP_CLIENT";
 #define WEB_URL "https://www.js-lottery.com"
 
 static const char *REQUEST = "GET " WEB_URL " HTTP/1.0\r\n"
-    "Host: "WEB_SERVER"\r\n"
-    "User-Agent: esp-idf/1.0 esp32\r\n"
-    "\r\n";
+    "Host: " WEB_SERVER "\r\n" "User-Agent: esp-idf/1.0 esp32\r\n" "\r\n";
 
 extern const unsigned char js_lottery_com_root_cert_pem_start[]
 asm("_binary_js_lottery_com_root_cert_pem_start");
@@ -36,96 +36,130 @@ static uint8_t led_v[] = {
     DIS_8, DIS_9
 };
 
-static int crawl_lottery_web(char *buf)
+static char *crawl_lottery_web(char *buf)
 {
-	int ret, len;
+    int ret, len;
 
-	esp_tls_cfg_t *cfg = malloc(sizeof(esp_tls_cfg_t));
-	cfg->cacert_pem_buf = js_lottery_com_root_cert_pem_start;
-	cfg->cacert_pem_bytes = js_lottery_com_root_cert_pem_end - js_lottery_com_root_cert_pem_start;
+    char *data = NULL;
+    const char dest[] = "fa fa-star";
 
-        //esp_tls_cfg_t cfg = {
-        //    .cacert_pem_buf  = js_lottery_com_root_cert_pem_start,
-        //    .cacert_pem_bytes = js_lottery_com_root_cert_pem_end - js_lottery_com_root_cert_pem_start,
-        //};
-        
-        struct esp_tls *tls = esp_tls_conn_http_new(WEB_URL, cfg);
-        
-        if(tls != NULL) {
-            ESP_LOGI(TAG, "Connection established...");
-        } else {
-            ESP_LOGE(TAG, "Connection failed...");
-	    ret = -1;
-            goto exit;
-        }
-        
-        size_t written_bytes = 0;
-        do {
-            ret = esp_tls_conn_write(tls, 
-                                     REQUEST + written_bytes, 
-                                     strlen(REQUEST) - written_bytes);
-            if (ret >= 0) {
-                ESP_LOGI(TAG, "%d bytes written", ret);
-                written_bytes += ret;
-            } else if (ret != ESP_TLS_ERR_SSL_WANT_READ  && ret != ESP_TLS_ERR_SSL_WANT_WRITE)
-            {
-                ESP_LOGE(TAG, "esp_tls_conn_write  returned 0x%x", ret);
-                goto exit;
-            }
-        } while(written_bytes < strlen(REQUEST));
+    esp_tls_cfg_t *cfg = malloc(sizeof(esp_tls_cfg_t));
+    cfg->cacert_pem_buf = js_lottery_com_root_cert_pem_start;
+    cfg->cacert_pem_bytes =
+	js_lottery_com_root_cert_pem_end -
+	js_lottery_com_root_cert_pem_start;
 
-        ESP_LOGI(TAG, "Reading HTTP response...");
+    //esp_tls_cfg_t cfg = {
+    //    .cacert_pem_buf  = js_lottery_com_root_cert_pem_start,
+    //    .cacert_pem_bytes = js_lottery_com_root_cert_pem_end - js_lottery_com_root_cert_pem_start,
+    //};
 
-        do
-        {
-            len = MAX_HTTP_OUTPUT_BUFFER - 1;
-            bzero(buf, MAX_HTTP_OUTPUT_BUFFER);
-            ret = esp_tls_conn_read(tls, (char *)buf, len);
+    struct esp_tls *tls = esp_tls_conn_http_new(WEB_URL, cfg);
 
-            if (ret == ESP_TLS_ERR_SSL_WANT_READ  || ret == ESP_TLS_ERR_SSL_WANT_WRITE)
-                continue;
-            
-            if(ret < 0)
-            {
-                ESP_LOGE(TAG, "esp_tls_conn_read  returned -0x%x", -ret);
-                break;
-            }
+    if (tls != NULL) {
+	ESP_LOGI(TAG, "Connection established...");
+    } else {
+	ESP_LOGE(TAG, "Connection failed...");
+	data = NULL;
+	goto exit;
+    }
 
-            if(ret == 0)
-            {
-                ESP_LOGI(TAG, "connection closed");
-                break;
-            }
+    size_t written_bytes = 0;
+    do {
+	ret =
+	    esp_tls_conn_write(tls, REQUEST + written_bytes,
+			       strlen(REQUEST) - written_bytes);
+	if (ret >= 0) {
+	    ESP_LOGI(TAG, "%d bytes written", ret);
+	    written_bytes += ret;
+	} else if (ret != ESP_TLS_ERR_SSL_WANT_READ
+		   && ret != ESP_TLS_ERR_SSL_WANT_WRITE) {
+	    ESP_LOGE(TAG, "esp_tls_conn_write  returned 0x%x", ret);
+	    goto exit;
+	}
+    } while (written_bytes < strlen(REQUEST));
 
-            len = ret;
-            ESP_LOGD(TAG, "%d bytes read", len);
-            /* Print response directly to stdout as it is read */
-            for(int i = 0; i < len; i++) {
-                putchar(buf[i]);
-            }
-	    ret = 0;
-        } while(1);
+    ESP_LOGI(TAG, "Reading HTTP response...");
 
-    exit:
-	free(cfg);
-        esp_tls_conn_delete(tls);    
-        putchar('\n'); // JSON output doesn't have a newline at end
+    do {
+	len = MAX_HTTP_OUTPUT_BUFFER - 1;
+	bzero(buf, MAX_HTTP_OUTPUT_BUFFER);
+	ret = esp_tls_conn_read(tls, (char *) buf, len);
 
-	return ret;
+	if (ret == ESP_TLS_ERR_SSL_WANT_READ
+	    || ret == ESP_TLS_ERR_SSL_WANT_WRITE)
+	    continue;
+
+	if (ret < 0) {
+	    ESP_LOGE(TAG, "esp_tls_conn_read  returned -0x%x", -ret);
+	    data = NULL;
+	    break;
+	}
+
+	if (ret == 0) {
+	    ESP_LOGI(TAG, "connection closed");
+	    break;
+	}
+
+	len = ret;
+	ESP_LOGD(TAG, "%d bytes read", len);
+	/* Print response directly to stdout as it is read */
+	// for(int i = 0; i < len; i++) {
+	//     putchar(buf[i]);
+	// }
+	if ((data = strstr(buf, dest)) != NULL) {
+	    ESP_LOGI(TAG, "lottery data have been fetched");
+	    for (int i = 0; i < 100; i++) {
+		putchar(data[i]);
+	    }
+	    break;
+	}
+    } while (1);
+
+  exit:
+    free(cfg);
+    esp_tls_conn_delete(tls);
+    putchar('\n');		// JSON output doesn't have a newline at end
+
+    return data;
 }
-
 
 // return the amount e.g. 8.76
-static void extract_lottery_amount(char *web_page, char *amount)
+static char *extract_lottery_amount(char *lottery_data)
 {
-    //...
-    strcpy(amount, "8.36");
+    const char *color_id = "color";
+    char *color_pos = NULL;
+    char *amount = NULL;
+    int len = 0 if ((color_pos = strstr(lottery_data, color_id)) != NULL) {
+	ESP_LOGI(TAG, "got color identifier");
+
+	/* I analyzed it, no more 50 bytes to get amount data */
+	for (int i = 0; i < 50; i++) {
+	    if (color_pos[i] == '>') {
+		if (color_pos[i + 1] >= '0' && color_pos[i + 1] <= '9')
+		    amount = color_pos + i + 1;
+		/*
+		 * I suppose '亿' is encoded as UTF-8, that is [0xe4, 0xba, 0xbf].
+		 * Because esp8266's SDK is good enough! :)
+		 */
+	    } else if (color_pos[i] == 0xe4 && color_pos[i + 1] == 0xba
+		       && color_pos[i + 2] == 0xbf) {
+		color_pos[i] = '\0';
+		// Tell me it's right.
+		ESP_LOGI(TAG, "the amount is '%s'", amount);
+		return amount;
+	    } else {
+	    }
+	}
+    } else {
+	ESP_LOGE(TAG, "wrong lottery_data");
+	printf("%s\n", lottery_data);
+	return NULL;
+    }
 }
 
-// e.g. amount is 8.76 or 10.33
-static void amount_to_led_bits(char *amount, dis_val_t * dis_val)
+static void amount_to_led_bits(char *amount, dis_val_t *dis_val)
 {
-    // "8.76"   
     int i;
     char b;
     int dot = 0;
@@ -148,32 +182,34 @@ static void amount_to_led_bits(char *amount, dis_val_t * dis_val)
 static void crawl_lottery_data_task(void *arg)
 {
     dis_val_t *dis_val = (dis_val_t *) arg;
-    char amount[8] = { 0 };
+    char *amount;
+    char *lottery_data;
     // task stack is not sufficient, use heap by malloc
     char *web_page = malloc(MAX_HTTP_OUTPUT_BUFFER);
     memset(web_page, 0, MAX_HTTP_OUTPUT_BUFFER);
 
-    /* loop forever per 1s */
+    /* loop forever per CRAWL_PER_SEC */
     while (1) {
-	//
-	if (crawl_lottery_web(web_page)) {
-	    continue;
+	if ((lottery_data = crawl_lottery_web(web_page)) == NULL) {
+	    continue;		// shouldn't occur
 	}
-	extract_lottery_amount(web_page, amount);
+	if ((amount = extract_lottery_amount(lottery_data)) == NULL) {
+	    continue;		// shouldn't occur
+	}
 	amount_to_led_bits(amount, dis_val);
-        ESP_LOG_BUFFER_HEX(TAG, dis_val, 4);
-	// delay 1s
-	vTaskDelay(1000 / portTICK_RATE_MS);
+	// show me the led code data
+	ESP_LOG_BUFFER_HEX(TAG, dis_val, 4);
+	memset(web_page, 0, MAX_HTTP_OUTPUT_BUFFER);
+	vTaskDelay(CRAWL_PER_SEC * 1000 / portTICK_RATE_MS);
     }
 
     free(web_page);
 }
 
-void crawl_lottery_data(dis_val_t * dis_val)
+void crawl_lottery_data(dis_val_t *dis_val)
 {
 #define TASK_STACK_SIZE 4096
 #define TASK_PRIORTY 10		// Low priority numbers denote low priority tasks
-    //TaskHandle_t task_handle = NULL;
 
     xTaskCreate(&crawl_lottery_data_task, "crawl_lottery_data_task",
 		TASK_STACK_SIZE, (void *) dis_val, TASK_PRIORTY, NULL);
